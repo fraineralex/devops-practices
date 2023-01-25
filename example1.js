01c2af61f19e6a4dd3a8884615d52d1e6b514e31
Frainer Encarnación <fraineralex2001@gmail.com>
2023-01-25 13:16:48 -0400

    [FEAT] update the file example1.js before review all changes in the staging branch

--- example1.js
+++ example1.js
@@ -1,3 +1,3 @@
-//This is the example 1 confirmed in the staging branch.
+//This is the example 1 confirmed ansd reviewed in the master branch.
 
-console.log('This is the example 1 after comfirm changes located in staging branch')
+console.log('This is the example 1 after comfirm  and review all changes, It is located in master branch')
