b545a5d2fc5f27d78ab1087daedd8f0c4afafcd3
Frainer Encarnación <fraineralex2001@gmail.com>
2023-02-10 17:41:09 -0400

    [FEAT] add the docker settings to run a container with python and postgres

---
+++ tarea2-Docker/main.py
@@ -0,0 +1,18 @@
+print(
+    """
+        ------------------
+           Hello World!!!  
+        ------------------ 
+    - We have successfully run python with psotgres
+    - You can manager the postgres database here: http://localhost:8080/
+    
+    ------------------------------------
+        Login information to adminer
+    ------------------------------------
+    - System: PostgresSQL
+    - Server: database
+    - Usernmae: developer
+    - Password: 1234
+    - Database: devops
+    """
+)
